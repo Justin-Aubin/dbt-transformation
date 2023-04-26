543fd79cf7b3ec4257c662cc96c528df8f108192
Justin AUBIN <jaubin@ippon.fr>
2023-04-26 15:42:14 +0200

    [+] Add colomn status

--- models/camel_date_transformed.sql
+++ models/camel_date_transformed.sql
@@ -3,6 +3,7 @@ select
 	firstname,
 	lastname,
 	to_char(startdate :: date,'DD-MM-YYYY, 9:00:00') as startdate,
-	to_char(enddate :: date,'DD-MM-YYYY, 18:00:00') as enddate
+	to_char(enddate :: date,'DD-MM-YYYY, 18:00:00') as enddate,
+	'VACATIONS' as status
 from 
 	public.camel_stream
